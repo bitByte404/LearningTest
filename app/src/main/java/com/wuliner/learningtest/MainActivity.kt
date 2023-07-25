3d21e560af5b2720c42603a257defd730f630a5a
wuliner <3302089539@qq.com>
2023-07-25 20:14:34 +0800

    dev_ViewModel：使用ononSavaInstance方法保存数据

--- app/src/main/java/com/wuliner/learningtest/MainActivity.kt
+++ app/src/main/java/com/wuliner/learningtest/MainActivity.kt
@@ -2,10 +2,31 @@ package com.wuliner.learningtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.wuliner.learningtest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var number = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //取出保存的值，显示到界面
+        if (savedInstanceState != null) {
+           number = savedInstanceState.getInt("numberKey")
+            binding.result.text = number.toString()
+        }
+        binding.addButton.setOnClickListener {
+            number++
+            binding.result.text = number.toString()
+        }
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt("numberKey", number)
+        super.onSaveInstanceState(outState)
     }
 }
